b2e9d07ea598e579ef08d244e048a779f421b7a8
vinayteja-a <vinayachari11@gmail.com>
2025-10-04 17:53:35 +0530

    Initial commit

---
+++ force-app/main/default/lwc/accountContacts/accountContacts.js
@@ -0,0 +1,19 @@
+import { LightningElement, api, wire } from 'lwc';
+import getContactsByAccountId from '@salesforce/apex/AccountContactService.getContactsByAccountId';
+
+export default class AccountContacts extends LightningElement {
+    @api recordId; // Account Id passed automatically if placed on Account record page
+    contacts;
+    error;
+
+    @wire(getContactsByAccountId, { accountId: '$recordId' })
+    wiredContacts({ error, data }) {
+        if (data) {
+            this.contacts = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.contacts = undefined;
+        }
+    }
+}
